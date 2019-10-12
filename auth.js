01df929380dfe62185315a6504b5421d976f5772
Hotmonk <avinashkhetri@gmail.com>
2019-10-12 11:47:44 +0530

    bus-lo

---
+++ auth.js
@@ -0,0 +1,12 @@
+//signup
+
+// const signupform= document.querySelector('#signupform');
+
+signupform.addEventListener("submit",(e)=>{
+     e.preventDefault();
+//     const email=signupform['signupemail'].value;
+//     const password=signupform['signuppassword'].value;
+//     console.log(email);
+//     console.log(password);
+// })
+
